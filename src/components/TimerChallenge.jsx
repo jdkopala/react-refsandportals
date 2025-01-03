960175505b525272da7f028a85de278c38f75040
Jesse Kopala <jkopala@atrematech.com>
2025-01-03 15:50:09 -0700

    use ref to store timer

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 export default function TimerChallenge({ title, targetTime }) {
+    const timer = useRef();
+
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     function handleStart() {
         setTimerExpired(false);
         setTimerStarted(true);
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
             setTimerStarted(false);
         }, targetTime * 1000);
+    };
+
+    function handleStop() {
+        clearTimeout(timer.current);
     };
 
 
@@ -23,8 +29,8 @@ export default function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={handleStart}>
-                    {timerStarted ? 'Stop' : 'Start'}
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
             <p className={timerStarted ? 'active' : undefined}>
