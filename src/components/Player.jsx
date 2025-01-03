41064c589a153e350b29057965f9be7dc39c42ea
Jesse Kopala <jkopala@atrematech.com>
2025-01-03 11:45:40 -0700

    add two way binding and state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setSubmitted(false);
+    setPlayerName(e.target.value);
+  };
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : 'Unknown Entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
