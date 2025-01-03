3faf21baece57de3088fad8c17c025aa142fbc12
Jesse Kopala <jkopala@atrematech.com>
2025-01-03 15:41:09 -0700

    stub logic for timer game

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
+
+
 export default function TimerChallenge({ title, targetTime }) {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimerExpired(false);
+        setTimerStarted(true);
+        setTimeout(() => {
+            setTimerExpired(true);
+            setTimerStarted(false);
+        }, targetTime * 1000);
+    };
+
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'}
                 </button>
             </p>
-            <p className="">
-                Time is running.... / Timer Inactive
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer Inactive'}
             </p>
         </section>
     )
