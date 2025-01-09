252e16a33aca47e640dce0463c284e9c32dacf4c
Jesse Kopala <jkopala@atrematech.com>
2025-01-09 14:36:08 -0700

    share state across result modal and timer challenge

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 // This is how we use a ref across components pre React 19:
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining, onReset }, ref) {
     const dialog = useRef();
+
+    const userLost = timeRemaining <= 0
+    const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2)
 
     // This function can expose callable functions to other components
     useImperativeHandle(ref, () => {
@@ -15,10 +18,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 
     return (
         <dialog ref={dialog} className="result-modal">
-            <h2>You {result}!</h2>
+            {userLost && <h2>You Lost!</h2>}
             <p>The target time was <strong>{targetTime} seconds</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formattedTimeRemaining} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
